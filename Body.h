ad8611bcae232787835926a7722294043b3fbe7e
Constantin Nistor <cnistor86@gmail.com>
2017-04-13 14:47:16 +0300

    some source simplification

--- Body.h
+++ Body.h
@@ -8,20 +8,13 @@
 #ifndef SRC_MATH_BODY_H_
 #define SRC_MATH_BODY_H_
 
-#include"line.h"
-
 class Body {
-	vec2d _pos;
-	vec2d _vel;
-	float _mass;
-	line _line;
+
 public:
-	Body(vec2d pos = vec2d(0,0), vec2d vel = vec2d(0,0), float mass = 0);
-	virtual ~Body();
-	void move(float dt = 1);
-	void gravitateTo(const Body& b);
-	float distTo(const Body& b);
-	void draw();
+	Body(){/* */}
+	virtual ~Body(){/* */}
+	virtual void draw() = 0;
+	virtual void update(Body* b,float dt = 0){/* */}
 };
 
 #endif /* SRC_MATH_BODY_H_ */
